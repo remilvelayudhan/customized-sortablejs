9afffae79a3080985355c91a784837a5d16970aa
cvremil1 <remil.cv@infolitz.com>
2023-08-08 17:13:57 +0530

    completed the task with one bug "drag multiple element issue"

--- src/app/arrange/abstract-arrange.directive.ts
+++ src/app/arrange/abstract-arrange.directive.ts
@@ -1,8 +1,52 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: 'abstract-arrange',
 })
 export class AbstractArrangeDirective {
   test = 'this is a test';
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+ 
+ 
+  @HostListener('dragstart', ['$event'])
+  onDragStart(event: DragEvent) {
+    const target = event.target as HTMLElement;
+    this.renderer.addClass(event.target, 'dragging');
+    event.dataTransfer?.setData('text/plain', target.innerHTML);
+  }
+
+  @HostListener('dragend',['$event']) onDragEnd(event: DragEvent) {
+    this.renderer.removeClass(event?.target, 'dragging');
+  }
+
+  @HostListener('dragover', ['$event'])
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.renderer.addClass(event?.target, 'drag-over');
+  }
+
+  @HostListener('dragleave',['$event'])
+  onDragLeave(event: DragEvent) {
+    this.renderer.removeClass(event?.target, 'drag-over');
+  }
+
+
+  @HostListener('drop', ['$event']) onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.renderer.removeClass(event.target, 'drag-over'); 
+    const data = event.dataTransfer?.getData('text/plain');
+    if (data) {
+      const newItem = document.createElement('div');
+      newItem.textContent = data;
+      this.renderer.addClass(newItem, 'draggable');
+      this.renderer.addClass(newItem, 'list-group-item');
+      this.renderer.setAttribute(newItem, 'draggable', 'true'); 
+      this.renderer.appendChild(event.target, newItem);
+
+      const originalDraggable = document.querySelector('.dragging');
+      originalDraggable && originalDraggable.remove();
+      
+    }
+  }
+
 }
